879e926a72f13ade5aa9743487c8929c288f20b0
scivision <scivision@users.noreply.github.com>
2015-02-15 01:41:28 -0500

    image/video write demos

--- tifftest.py
+++ tifftest.py
@@ -1,7 +1,6 @@
 from skimage.io._plugins import freeimage_plugin as freeimg
+import numpy as np
 
+x = (np.random.rand(10,512,512)*255).astype(np.uint8)
 
-
-
-
-freeimg.write_multipage(frames, '/tmp/junk.tif')
+freeimg.write_multipage(x, '/tmp/junk.tif')
--- videowritetest.py
+++ videowritetest.py
@@ -1,15 +1,36 @@
+#!/usr/bin/python2
 """
-Not working 2015-Feb-13
-need to custom compile opencv with ffmpeg?
+Note: VideoWriter expects dimensions (x,y,3) and will fail otherwise,writing a tiny file perhaps
+Remember, VLC has a long-standing bug where files under about 3fps don't playback
+
+Note: the isColor parameter of VideoWriter works on Linux!
 """
 import cv2
 import numpy as np
 
-fourcc = cv2.cv.FOURCC('M','J','P','G')
-hv = cv2.VideoWriter('/tmp/test.avi',fourcc,fps=10.0,frameSize=(512,512))#,isColor=0)
-print(hv.isOpened())
-x = np.random.rand(512,512)
-hv.write(x)
-hv.write(x)
+usecolor = False
+
+#*** all of these codecs worked for me on Linux ***
+#fourcc = cv2.cv.FOURCC(*'MJPG') #.avi
+#fourcc = cv2.cv.FOURCC(*'XVID')
+fourcc = cv2.cv.FOURCC(*'FFV1')
+#fourcc = cv2.cv.FOURCC(*'IYUV')
+#fourcc = cv2.cv.FOURCC(*'THEO') #must name file .ogv, NOT .avi
+#*** these codecs did NOT work for me on Linux ***
+#fourcc = cv2.cv.FOURCC(*'CVID') #encoder not found
+#fourcc = cv2.cv.FOURCC(*'MJ2C') #segmentation fault
+#********************
+hv = cv2.VideoWriter('/tmp/test.avi',fourcc,
+                     fps=3, frameSize=(512,512), isColor=usecolor)
+if not hv.isOpened():
+    exit('*** trouble starting video file')
+
+if usecolor:
+    vdim = np.random.rand(100,512,512,3)
+else:
+    vdim = np.random.rand(100,512,512)
+vid = (vdim*255).astype(np.uint8)
+for v in vid:
+    hv.write(v)
 
 hv.release()
