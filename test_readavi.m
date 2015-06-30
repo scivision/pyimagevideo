f1895e7b272b18a1b7962aa20ff7c21fe9621302
scivision <scivision@users.noreply.github.com>
2015-06-30 14:10:12 -0400

    doc

--- test_readavi.m
+++ test_readavi.m
@@ -1,14 +1,19 @@
 %% test_readavi
 % demo of loading an AVI and displaying the first frame of video
 % helps determine if Matlab has a particular codec available
+% 
+%
 % Michael Hirsch
+%% Example: Matlab R2015a cannot read Cinepak encoded video.
+% (1) I converted Cinepak AVI to uncompressed AVI with Linux Terminal command:
+% ffmpeg -i ~/U/eng_research_irs/Auroral_Video/sCMOS_PFRR_Mar2012/CMOS_110302_0819.avi -vcodec rawvideo ~/CMOS_110302_0819.avi
+% (2) I ran this Matlab program and it displayed the first frame of video successfully
+%
+% Note: uncompressed AVI file can be 20 times filesize of Cinepak AVI
 
 function test_readavi()
 
-% uncompressed file can be 20 times size of original (ouch says the hard drive)
-% converted CinePak avi to uncompressed avi with command:
-% ffmpeg -i ~/U/eng_research_irs/Auroral_Video/sCMOS_PFRR_Mar2012/CMOS_110302_0819.avi -vcodec rawvideo ~/CMOS_110302_0819.avi
-fn = '~/CMOS_110302_0819.avi'; % "works for me" with Matlab R2015a on Linux
+fn = '~/CMOS_110302_0819.avi'; % uncompressed AVI "works for me" with Matlab R2015a on Linux
 showFirstFrame(fn)
 end
 
