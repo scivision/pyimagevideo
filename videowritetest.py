81cecee14901ba3599a3b668a38268a8022d9207
scivision <scivision@users.noreply.github.com>
2015-08-27 07:00:22 -0400

    smarter import

--- videowritetest.py
+++ videowritetest.py
@@ -6,6 +6,11 @@ Remember, VLC has a long-standing bug where files under about 3fps don't playbac
 Note: the isColor parameter of VideoWriter works on Linux!
 """
 import cv2
+try:
+    from cv2.cv import FOURCC as fourcc #Windows needs from cv2.cv
+except ImportError:
+    from cv2 import VideoWriter_fourcc as fourcc
+
 import numpy as np
 from tempfile import gettempdir
 from os.path import join
@@ -16,31 +21,31 @@ xpix=ypix=256
 
 ext='.avi'
 #*** all of these codecs worked for me on Ubuntu 14.04 and 15.04***
-fourcc = cv2.cv.FOURCC(*'MJPG') #works in 15.04,14.04 plays wherever
-#fourcc = cv2.cv.FOURCC(*'XVID') #works in 15.04,14.04 plays wherever
-#fourcc = cv2.cv.FOURCC(*'FFV1') #works in 15.04 and 14.04, use VLC or FFPLAY  to playback
+cc4 = fourcc(*'MJPG') #works in 15.04,14.04 plays wherever
+#cc4 = fourcc(*'XVID') #works in 15.04,14.04 plays wherever
+#fourcc = fourcc(*'FFV1') #works in 15.04 and 14.04, use VLC or FFPLAY  to playback
 
 #** maybe works somewhat
-#fourcc = cv2.cv.FOURCC(*'IYUV') # works 14.04, not work 15.04
-#fourcc = cv2.cv.FOURCC(*'THEO'); ext='.ogv' #must name file .ogv, NOT .avi\  -- somewhat broken, per messages in ffplay
+#fourcc = fourcc(*'IYUV') # works 14.04, not work 15.04
+#fourcc = fourcc(*'THEO'); ext='.ogv' #must name file .ogv, NOT .avi\  -- somewhat broken, per messages in ffplay
 
 #*** works on 15.04 but not 14.04
-#fourcc = cv2.cv.FOURCC(*'YV12') # 14.04 writes file, but file is unreadable "improper image header" -- 15.04 video works
+#fourcc = fourcc(*'YV12') # 14.04 writes file, but file is unreadable "improper image header" -- 15.04 video works
 
 #*** these codecs did NOT work for me on Ubuntu 14.04 or 15.04 ***
-#fourcc = cv2.cv.FOURCC(*'Y41P') #silent error, no write
-#fourcc = cv2.cv.FOURCC(*'YUV9') #silent error, no write -- 15.04 writes but nobody knows how to play
-#fourcc = cv2.cv.FOURCC(*'DIB ') # silent error, no write
-#fourcc = cv2.cv.FOURCC(*'CVID') #encoder not found
-#fourcc = cv2.cv.FOURCC(*'MJ2C') #segmentation fault -- 15.04 blank video
+#fourcc = fourcc(*'Y41P') #silent error, no write
+#fourcc = fourcc(*'YUV9') #silent error, no write -- 15.04 writes but nobody knows how to play
+#fourcc = fourcc(*'DIB ') # silent error, no write
+#fourcc = fourcc(*'CVID') #encoder not found
+#fourcc = fourcc(*'MJ2C') #segmentation fault -- 15.04 blank video
 #********************
 ofn = join(gettempdir(),'test'+ext)
 print('saving to ' + ofn)
 
-hv = cv2.VideoWriter(ofn,fourcc, fps=5, frameSize=(xpix,ypix), isColor=usecolor)
+hv = cv2.VideoWriter(ofn,cc4, fps=5, frameSize=(xpix,ypix), isColor=usecolor)
 
 if not hv.isOpened():
-    exit('*** trouble starting video file')
+    raise TypeError('trouble starting video file')
 
 if usecolor:
     vdim = np.random.rand(nframe,ypix,xpix,3)
