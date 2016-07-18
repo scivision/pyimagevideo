b2f005126a40c6c31fbfa39887f40797fe08167d
scivision <scivision@users.noreply.github.com>
2016-07-18 13:59:21 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,18 +4,12 @@ from setuptools import setup
 import subprocess
 
 try:
-    subprocess.run(['conda','install','--yes','--file','requirements.txt'],shell=False) #don't use os.environ
+    subprocess.call(['conda','install','--file','requirements.txt'],shell=False) #don't use os.environ
 except Exception as e:
-    print('you will need to install packages in requirements.txt  {}'.format(e))
-
-
-with open('README.rst','r') as f:
-	long_description = f.read()
+    pass
 
 setup(name='pyimagevideo',
-      version='0.1',
 	  description='utilites for reading,writing,plotting all kinds of images and video date',
-	  long_description=long_description,
 	  author='Michael Hirsch',
 	  url='https://github.com/scienceopen/pyimagevideo',
            packages=['pyimagevideo'],
