5f179d934f3a4b91f8de3a18f4694053180893e5
scivision <scivision@users.noreply.github.com>
2016-07-29 17:42:26 -0400

    doc, use invisible plot

--- matplotlib_avi.py
+++ matplotlib_avi.py
@@ -1,13 +1,18 @@
+#!/usr/bin/env python
 """
 example writing matplotlib movies with matlab "grabframe"-like syntax
 
 Just four lines of code to directly write lossless AVIs from Matplotlib
 lines 22-24,29 are all that's needed.
 """
+import matplotlib
+matplotlib.use('agg') #invisible while plotting, but faster and more robust
 from matplotlib.pyplot import figure,draw,pause
 import matplotlib.animation as anim
 from tempfile import mkstemp
 from numpy.random import uniform
+
+DPI=100
 
 fn = mkstemp('.avi','lossless')[1]
 
@@ -19,9 +24,10 @@ fg = figure()
 ax = fg.gca()
 hi = ax.imshow(uniform(size=imsize))
 #%% add writer context and grabframe to typical imshow loop
+#Writer = anim.writers['mencoder']
 Writer = anim.writers['ffmpeg']
 writer = Writer(fps=15, codec='ffv1')
-with writer.saving(fg, fn,100):
+with writer.saving(fg, fn, DPI):
     print('writing {}'.format(fn))
     for _ in range(50):
         hi.set_data(uniform(size=imsize))
