390915fab7fc685ad3dd928d3e890401588c8233
scivision <scivision@users.noreply.github.com>
2016-08-12 11:23:12 -0400

    reusable example

--- matplotlib_avi.py
+++ matplotlib_avi.py
@@ -19,39 +19,49 @@ import matplotlib.animation as anim
 from numpy.random import uniform
 
 DPI=100
-IMXY=(128,128)
+X=Y=128; N=50
 FPS=15  # keep greater than 3 to avoid VLC playback bug
 WRITER = 'ffmpeg'
 
-def setupfig(shape):
+def testdata(x,y,N):
+    # TODO: better example data to show downsides of lossy video
+    return uniform(size=(N,y,x))
+
+def setupfig(img,title=''):
     #%% boilerplate for making imshow priming (used in any program)
-    assert len(shape)==2,'2-D image expected'
+    assert img.ndim==2,'2-D image expected'
     fg = figure()
     ax = fg.gca()
-    h = ax.imshow(uniform(size=shape))
+    h = ax.imshow(img) #prime figure with first frame of data
+    ax.set_title(title,color='g')
     return fg,h
 
-def loop(fg,h,w,fn):
+def loop(fg,h,w,fn,imgs):
+    assert imgs.ndim in (3,4),'assuming image stack iterating over first dimension Nframe x X x Y [x RGB]'
+
     with w.saving(fg, fn, DPI):
         print('writing {}'.format(fn))
-        for _ in range(50):
-            h.set_data(uniform(size=IMXY))
+        for I in imgs:
+            h.set_data(I)
             draw()
             # NOTE: pause is NEEDED for more complicated plots, to avoid random crashes
             pause(0.01)
             w.grab_frame(facecolor='k')
+
+    close(fg)
+
 
 def config(h,codec):
     Writer = anim.writers[WRITER]
     return Writer(fps=FPS, codec=codec)
 
 if __name__ == '__main__':
-    fg,h = setupfig(IMXY)
+    imgs = testdata(X,Y,N)
 #%% lossless
+    fg,h = setupfig(imgs[0],'lossless')
     w = config(h,'ffv1')
-    loop(fg,h,w,'lossless.avi')
+    loop(fg,h,w,'lossless.avi',imgs)
 #%% lossy
+    fg,h = setupfig(imgs[0],'lossy')
     w = config(h,'mpeg4')
-    loop(fg,h,w,'lossy.avi')
-#%% cleanup invisible figure
-    close('all')
+    loop(fg,h,w,'lossy.avi',imgs)
