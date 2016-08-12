1a8f38e1e32f23f02cbc7d5d818685e9af65096e
scivision <scivision@users.noreply.github.com>
2016-08-12 11:10:58 -0400

    functionalize

--- matplotlib_avi.py
+++ matplotlib_avi.py
@@ -3,38 +3,58 @@
 example writing matplotlib movies with matlab "grabframe"-like syntax
 
 Just four lines of code to directly write lossless AVIs from Matplotlib
+
+http://matplotlib.org/api/animation_api.html#matplotlib.animation.FFMpegWriter
+
+codecs: 
+ffv1: lossless
+mpeg4: lossy
+
 """
 import matplotlib
 matplotlib.use('agg') #invisible while plotting, but faster and more robust
-from matplotlib.pyplot import figure,draw,pause
+from matplotlib.pyplot import figure,draw,pause,close
 import matplotlib.animation as anim
-from tempfile import mkstemp
+#
 from numpy.random import uniform
 
 DPI=100
+IMXY=(128,128)
+FPS=15  # keep greater than 3 to avoid VLC playback bug
 
-fn = mkstemp('.avi','lossless')[1]
+def setupfig():
+    #%% boilerplate for making imshow priming (used in any program)
+    fg = figure()
+    ax = fg.gca()
+    h = ax.imshow(uniform(size=IMXY))
+    return fg,h
 
-#%% random image setup
-imsize = (128,128)
-Nframe = 20
-#%% boilerplate for making imshow priming (used in any program)
-fg = figure()
-ax = fg.gca()
-hi = ax.imshow(uniform(size=imsize))
-#%% add writer context and grabframe to typical imshow loop"
-"""
-http://matplotlib.org/api/animation_api.html#matplotlib.animation.FFMpegWriter
-codec: ffv1 is lossless.
-lots more options are available.
-"""
-#Writer = anim.writers['mencoder']
-Writer = anim.writers['ffmpeg']
-writer = Writer(fps=15, codec='ffv1')
-with writer.saving(fg, fn, DPI):
-    print('writing {}'.format(fn))
-    for _ in range(50):
-        hi.set_data(uniform(size=imsize))
-        draw()
-        pause(0.01) # pause is sometimes needed for more complicated plots, to avoid random crashes
-        writer.grab_frame(facecolor='k')
+def loop(fg,h,w,fn):
+    with w.saving(fg, fn, DPI):
+        print('writing {}'.format(fn))
+        for _ in range(50):
+            h.set_data(uniform(size=IMXY))
+            draw()
+            pause(0.01) # pause is NEEDED for more complicated plots, to avoid random crashes
+            w.grab_frame(facecolor='k')
+
+def config(h,codec):
+    #Writer = anim.writers['mencoder'] #usually FFMPEG works
+    Writer = anim.writers['ffmpeg']
+    return Writer(fps=FPS, codec=codec)
+
+if __name__ == '__main__':
+    fg,h = setupfig()
+    losslessfn = 'lossless.avi'
+    lossyfn = 'lossy.avi'
+
+#%% lossless
+    w = config(h,'ffv1')
+    loop(fg,h,w,losslessfn)
+
+#%% lossy
+    w = config(h,'mpeg4')
+    loop(fg,h,w,lossyfn)
+
+#%% cleanup invisible figure
+    close('all')  
