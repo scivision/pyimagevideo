edcb4768031e35a7728b94a1233965b07d2bca8e
scivision <scivision@users.noreply.github.com>
2016-12-28 21:03:01 -0500

    imp [ci skip]

--- Demo_findpeaks.py
+++ Demo_findpeaks.py
@@ -3,7 +3,7 @@
 compare with demo_findpeaks.m
 """
 from numpy import array
-from scipy.signal import savgol_filter,argrelmax
+from scipy.signal import argrelmax
 from matplotlib.pyplot import figure,show
 
 noisy = array([1,3,5,2,1,5,5.01,0,2,4,6,0,7,1,0])
