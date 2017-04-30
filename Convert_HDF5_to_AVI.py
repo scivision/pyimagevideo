cd909fa21cd5a8a0280a67450a063d1b11cf7789
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2017-04-30 19:31:22 -0400

    scipy bug

--- Convert_HDF5_to_AVI.py
+++ Convert_HDF5_to_AVI.py
@@ -6,7 +6,7 @@ Remember, VLC has a long-standing bug where files under about 3fps don't playbac
 Note: the isColor parameter of VideoWriter works on Linux too
 
 Example:
-./Convert_HDF5_to_AVI.py ~/data/2012-12-25/extracted.h5 ~/data/2012-12-25/ex.ogv THEO -minmax 30 2000
+./Convert_HDF5_to_AVI.py ~/data/2012-12-25/extracted.h5 -o ~/data/2012-12-25/ex.ogv -cc4 THEO
 
 Just get percentiles
 ./Convert_HDF5_to_AVI.py ~/data/2012-12-25/extracted.h5
@@ -15,12 +15,16 @@ Just get percentiles
 from pathlib import Path
 import h5py
 import numpy as np
-from scipy.misc import bytescale
+#from scipy.misc import bytescale BUGS
+#from scipy.signal import wiener
 #
+from histutils import sixteen2eight
 from pyimagevideo import videoWriter
 
 usecolor = False
-
+window = 100 # number of frames over which to auto contrast
+PTILE=[5, 99.95]
+wienernhood=3
 """
 all of these codecs worked for me on Ubuntu 14.04 and 16.04
 'MJPG' Motion JPEG
@@ -41,7 +45,7 @@ all of these codecs worked for me on Ubuntu 14.04 and 16.04
 'MJ2C' #segmentation fault -- 15.04 blank video
 """
 
-def hdf2avi(infn:Path, outfn:Path, h5key:str, cc4:str, mm,fps):
+def hdf2avi(infn:Path, outfn:Path, h5key:str, cc4:str, mm=None, fps=30,ptile=PTILE):
     """
     infn: HDF5 file containing video to read
     outfn: video file
@@ -60,21 +64,29 @@ def hdf2avi(infn:Path, outfn:Path, h5key:str, cc4:str, mm,fps):
         h = videoWriter(outfn, cc4, (x, y), fps, usecolor)
 # %% loop over HDF5 video
         for i,I in enumerate(f[h5key]):
-            if not i % 100:
-                print('{:.1f} %'.format(i/N*1000))
-            img = bytescale(I, mm[0], mm[1])
+            if not i % window:
+                f[h5key][i:i+window]
+                if mm is None:
+                    minmax = np.percentile(f[h5key][i:i+window], ptile, interpolation='nearest')
+                else:
+                    minmax = mm
+                print(f'{i/N*100:.1f} %  min/max {minmax}')
+
+ #           I = wiener(I,wienernhood)
+            #img = bytescale(I, minmax[0], minmax[1]) BUG
+            img = sixteen2eight(I, minmax)
             h.write(img)
 # %% close video
     h.release()
 
-def getprc(fn,key,stride=60,ptile=[.01,99.999]):
+def getprc(fn, key, stride=60, ptile=PTILE):
     """ plot some file statistics to help decide min/max"""
     fn = Path(fn).expanduser()
     fGB = fn.stat().st_size/1e9
     print(f'sampling {ptile} percentiles {fn}, reading {1/60*fGB:.1f} of {fGB:.1f} GB')
 
     with h5py.File(fn,'r',libver='latest') as f:
-        prc = np.percentile(f[key][::stride,...],ptile,interpolation='nearest')
+        prc = np.percentile(f[key][::stride,...], ptile, interpolation='nearest')
 
     print(f'percentiles {ptile}:  {prc}')
 
@@ -101,9 +113,9 @@ if __name__ == '__main__':
     p.add_argument('infn',help='HDF5 video file to read')
     p.add_argument('-o','--outfn',help='video file to write e.g. cool.avi')
     p.add_argument('-k','--h5key',help='key to HDF5 video (variable in HDF5 file)')
-    p.add_argument('cc4',help='video codec CC4 code',nargs='?',default='FMP4')
-    p.add_argument('-minmax',help='minimum, maximum values. default (0,65535)',type=int,nargs=2,default=(0,65535))
-    p.add_argument('-fps',help='frames/sec of output video',type=int,default=20)
+    p.add_argument('-cc4',help='video codec CC4 code',default='FMP4')
+    p.add_argument('-minmax',help='minimum, maximum values. Automatic if not specified.')
+    p.add_argument('-fps',help='frames/sec of output video',type=int,default=30)
     p = p.parse_args()
 
     h5key = findvidvar(p.infn) if p.h5key is None else p.h5key
@@ -111,4 +123,4 @@ if __name__ == '__main__':
     if not p.outfn:
         getprc(p.infn, h5key)
     else:
-        hdf2avi(p.infn, p.outfn, h5key, p.cc4, p.minmax,p.fps)
+        hdf2avi(p.infn, p.outfn, h5key, p.cc4, p.minmax, p.fps)
