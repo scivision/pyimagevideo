c6217c4e9e1e659efff93ab9c0d48f4a1b8a0c52
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2017-09-13 23:01:30 -0400

    step correct

--- Convert_HDF5_to_AVI.py
+++ Convert_HDF5_to_AVI.py
@@ -149,4 +149,4 @@ if __name__ == '__main__':
         getprc(p.infn, h5key)
         print('use -o to write file')
     else:
-        hdf2avi(p.infn, p.outfn, h5key, p.cc4, p.minmax, p.fps, p.step)
+        hdf2avi(p.infn, p.outfn, h5key, p.cc4, p.minmax, p.fps, step=p.step)
