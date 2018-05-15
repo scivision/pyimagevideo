08c40ffa339fcee53dfd92e4c729e55f71cf47c2
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2018-05-15 11:48:31 -0400

    optional cv2 test

--- tests/test_all.py
+++ tests/test_all.py
@@ -28,5 +28,24 @@ def test_tiff_multipage_rw():
 def test_wavelength2rgb():
     np.testing.assert_allclose(piv.wavelength2rgb(720),(146,0,0))
 
+def test_cv2codec_read():
+    try:
+        import cv2
+    except ImportError:
+        return
+        
+    fn = R/'tests/star_collapse_out.avi'
+    vid = cv2.VideoCapture(str(fn))
+
+    ret,img = vid.read() #a 3-D Numpy array, last axis is BGR: blue,green,red
+    vid.release()
+
+    assert ret,'could not open video'
+    assert img.shape == (480,720,3),'video not decoded properly'
+    
+    
+#    cv2.imshow(fn.name, img)
+ #   cv2.destroyAllWindows()
+    
 if __name__ == '__main__':
     np.testing.run_module_suite()
