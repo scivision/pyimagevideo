3eab42ef1441b6f41b6f741c0af2bf0cc8728956
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2019-10-01 23:09:05 -0400

    add contiguous print

--- RGB_BGR_GBR_conv.py
+++ RGB_BGR_GBR_conv.py
@@ -9,30 +9,30 @@ from pathlib import Path
 import imageio
 import matplotlib
 from matplotlib.pyplot import figure, show
+
 try:
     import cv2
 except ImportError:
     cv2 = None
 
 # %% 0. read test file
-file = Path(__file__).parent / 'tests/rgb.png'
+file = Path(__file__).parent / "tests/rgb.png"
 rgb = imageio.imread(file)
-
 # %% 1. show RGB, BGR, GBR in Matplotlib
 print("Matplotlib version", matplotlib.__version__)
 fg = figure(figsize=(15, 4))
 ax = fg.subplots(1, 3, sharey=True)
 
-ax[0].imshow(rgb, origin='upper')
-ax[0].set_title('RGB')
+ax[0].imshow(rgb, origin="upper")
+ax[0].set_title("RGB")
 
 bgr = rgb[..., ::-1]
-ax[1].imshow(bgr, origin='upper')
-ax[1].set_title('BGR')
+ax[1].imshow(bgr, origin="upper")
+ax[1].set_title("BGR")
 
 gbr = rgb[..., [2, 0, 1]]
-ax[2].imshow(gbr, origin='upper')
-ax[2].set_title('GBR')
+ax[2].imshow(gbr, origin="upper")
+ax[2].set_title("GBR")
 
 fg.suptitle(f"Color order top to bottom.   {file}")
 fg.tight_layout()
@@ -41,6 +41,9 @@ show()
 # %% 2. show RGB, BGR, GBR in OpenCV HighGUI
 if cv2 is None:
     raise SystemExit("OpenCV not available.")
+
+print("RGB C contiguous image array?", rgb.flags["C_CONTIGUOUS"])
+print("BGR C contiguous image array?", bgr.flags["C_CONTIGUOUS"])
 
 print("OpenCV version", cv2.__version__)
 cv2.imshow("RGB needs BGR array", bgr)
